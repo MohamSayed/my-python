b04f97a13604885076871d8a3843e94a0076ccc8
MohamSayed <me.dev6789@gmail.com>
2020-10-15 13:35:08 +0200

    mechnicalsoup_demo.py instead of mechanicalsoup.py

---
+++ libraries/mechanicalsoup_demo.py
@@ -0,0 +1,16 @@
+import mechanicalsoup
+browser = mechanicalsoup.StatefulBrowser(
+    soup_config={'features': 'lxml'},
+    raise_on_404=True,
+    user_agent='Mozilla/5.0',
+)
+# Uncomment for a more verbose output:
+# browser.set_verbose(2)
+URL = "https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page"
+browser.open(URL)
+form = browser.select_form()
+browser["wpName"] = ""
+browser["wpPassword"] = ""
+resp = browser.submit_selected()
+
+page = browser.get_current_page()
